cc0563dec0ec26002de952cf646bff911c689f18
Tharsikan <thar.navaratnam@gmail.com>
2022-02-03 16:18:58 -0500

    cleanup boilerplate

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+
 import './css/body.css';
 import './css/app.css';
 import './css/header.css';
 
 import App from './App.jsx';
 
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  , document.getElementById('app'));
