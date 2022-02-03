7f9d3e17123bd53de11e9fe56606cf1c52cb9a31
Tharsikan <thar.navaratnam@gmail.com>
2022-02-03 17:10:27 -0500

    add route for all filters

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+import useCallsData from './hooks/useCallsData.js';
 import Header from './Header.jsx';
+import CallsList from './CallsList.jsx';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
+  const { calls, setCalls } = useCallsData();
+
+  console.log(calls);
+  
   return (
     <div className='container'>
-      <Header/>
-      <div className="container-view">Some activities should be here</div>
+      <Header />
+      <div className="container-view">
+        <Routes>
+          <Route path="/" element={<CallsList {...{ calls }} />} />
+          <Route path="/archive" element={<CallsList {...{ calls, filter: "archive" }} />} />
+          <Route path="/inbound" element={<CallsList {...{ calls, filter: "inbound" }} />} />
+          <Route path="/outbound" element={<CallsList {...{ calls, filter: "outbound" }} />} />
+          <Route path="/missed" element={<CallsList {...{ calls, filter: "missed" }} />} />
+          <Route path="/call/:id" element={<div>Call Details</div>} />
+        </Routes>
+      </div>
     </div>
   );
 };
-
-ReactDOM.render(<App/>, document.getElementById('app'));
 
 export default App;
